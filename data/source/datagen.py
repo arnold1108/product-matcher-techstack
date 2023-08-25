fae555f12dfe7847612c669d3216a55977dd1650
opiyodhiambo <rnldlinus@gmail.com>
2023-08-25 16:24:35 +0300

    connecting to the db

--- data/source/datagen.py
+++ data/source/datagen.py
@@ -1,4 +1,15 @@
 from faker import Faker
+import psycopg2
 
 fake = Faker()
-print(fake.name())
+
+# Connecting to the PostgreSQL database
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    dbname="product_matcher",
+    user="adventure",
+    password="opiyo123",
+)
+
+# Generate and insert fake user data
