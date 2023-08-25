8bf052b304629ec13efff16a72dcc117ba5ba2e6
opiyodhiambo <rnldlinus@gmail.com>
2023-08-25 17:32:48 +0300

    published data into the db

--- data/source/datagen.py
+++ data/source/datagen.py
@@ -13,4 +13,31 @@ conn = psycopg2.connect(
 )
 
 print("Connected to the PostgreSQL database!")
+
+# creating a cusor
+cur = conn.cursor()
+
 # Generate and insert fake user data
+for _ in range(20):  # Generate 20 fake users
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = fake.email()
+    date_of_birth = fake.date_of_birth(minimum_age=18, maximum_age=80)
+    region = fake.country()
+
+    cur.execute(
+        "INSERT INTO users (first_name, last_name, email, data_of_birth, region)"
+        "VALUES (%s, %s, %s, %s, %s)",
+        (first_name, last_name, email, date_of_birth, region),
+    )
+
+# Confirming the inserted user data
+cur.execute("SELECT * FROM users")
+users = cur.fetchall()
+
+for user in users:
+    print(user)
+
+# Closing the connection
+cur.close()
+conn.close()
