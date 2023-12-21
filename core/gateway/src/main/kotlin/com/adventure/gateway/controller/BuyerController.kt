cc9f645422326ef68c04fc05c425769d1a98f56f
opiyodhiambo <rnldlinus@gmail.com>
2023-12-21 17:17:48 +0300

    add buyer controller

--- core/gateway/src/main/kotlin/com/adventure/gateway/controller/BuyerController.kt
+++ core/gateway/src/main/kotlin/com/adventure/gateway/controller/BuyerController.kt
@@ -1,13 +1,16 @@
 package com.adventure.gateway.controller
 
-import com.adventure.apis.accounts.Commands
 import com.adventure.apis.accounts.Commands.CreateBuyerAccount
-import com.adventure.apis.accounts.State
 import com.adventure.apis.accounts.State.Sex
-import com.adventure.apis.marketplace.QueryResults
+import com.adventure.apis.cart.Commands
+import com.adventure.apis.cart.Commands.*
+import com.adventure.apis.cart.QueryResults
+import com.adventure.apis.cart.QueryResults.ViewCartQueryResult
+import com.adventure.apis.marketplace.Commands.LikeProduct
 import com.adventure.apis.marketplace.QueryResults.ExploreProductsQueryResult
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -28,18 +31,70 @@ class BuyerController(
         emailAddress: String,
         gender: Sex
     ): Mono<ResponseEntity<String>> {
-        val buyerAccountCreateRequest = CreateBuyerAccount(
+        val createBuyerAccountCommand = CreateBuyerAccount(
             buyerId = UUID.randomUUID(),
             firstName = firstName,
             lastName = lastName,
             email = emailAddress,
             gender = gender
         )
-        return command.send(buyerAccountCreateRequest)
+        return command.send(createBuyerAccountCommand)
     }
+
     @GetMapping("/buyer/{buyerId}/explore")
     fun exploreProducts(@PathVariable buyerId: UUID): Mono<ResponseEntity<ExploreProductsQueryResult>> {
         TODO()
+    }
+
+    @PostMapping("/buyer/{buyerId}/explore/{productId}/like")
+    fun likeProduct(
+        @PathVariable productId: UUID,
+        @PathVariable buyerId: UUID
+    ): Mono<ResponseEntity<String>> {
+        val likeProductCommand = LikeProduct(
+            buyerId = buyerId,
+            productId = productId
+        )
+        return command.send(likeProductCommand)
+    }
+
+    @GetMapping("/buyer/{buyerId}/cart")
+    fun viewCart(@PathVariable buyerId: UUID): Mono<ResponseEntity<ViewCartQueryResult>> {
+        TODO()
+    }
+
+    @PostMapping("/buyer/{buyerId}/cart/add/{productId}")
+    fun addProductToCart(
+        @PathVariable buyerId: UUID,
+        @PathVariable productId: UUID,
+        quantity: Int
+        ): Mono<ResponseEntity<String>> {
+        val addProductToCartCommand = AddProductToCart(
+            buyerId = buyerId,
+            productId = productId,
+            quantity = quantity
+        )
+        return command.send(addProductToCartCommand)
+    }
+
+    @DeleteMapping("/buyer/{buyerId}/cart/remove/{productId}")
+    fun removeProductFromCart(
+        @PathVariable productId: UUID,
+        @PathVariable buyerId: UUID
+        ): Mono<ResponseEntity<String>> {
+        val removeProductFromCartCommand = RemoveProductFromCart(
+            productId = productId,
+            buyerId = buyerId
+        )
+        return command.send(removeProductFromCartCommand)
+    }
+
+    @PostMapping("/buyer/{buyerId}/cart/checkout")
+    fun checkout(@PathVariable buyerId: UUID): Mono<ResponseEntity<String>> {
+        val checkoutCommand = Checkout(
+            buyerId = buyerId
+        )
+        return command.send(checkoutCommand)
     }
 
 }
--- core/gateway/src/main/kotlin/com/adventure/gateway/controller/SellerController.kt
+++ core/gateway/src/main/kotlin/com/adventure/gateway/controller/SellerController.kt
@@ -1,18 +1,13 @@
 package com.adventure.gateway.controller
 
-import com.adventure.apis.accounts.Commands
 import com.adventure.apis.accounts.Commands.CreateSellerAccount
-import com.adventure.apis.accounts.State
 import com.adventure.apis.accounts.State.Sex
-import com.adventure.apis.store.Category
 import com.adventure.apis.store.Category.StoreCategory
 import com.adventure.apis.store.Commands.AddStock
 import com.adventure.apis.store.Commands.CreateStore
-import com.adventure.apis.store.QueryResults
 import com.adventure.apis.store.QueryResults.ManageStoreQueryResults
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -33,28 +28,30 @@ class SellerController(
         emailAddress: String,
         gender: Sex
     ): Mono<ResponseEntity<String>> {
-        val sellerAccountCreateRequest = CreateSellerAccount(
+        val createSellerAccountCommand = CreateSellerAccount(
             sellerId = UUID.randomUUID(),
             firstName = firstName,
             lastName = lastName,
             email = emailAddress,
             gender = gender
         )
-        return command.send(sellerAccountCreateRequest)
+        return command.send(createSellerAccountCommand)
     }
+
     @PostMapping("/seller/store/create")
     fun createStore(
         @RequestBody
         sellerId: UUID,
         category: StoreCategory
     ): Mono<ResponseEntity<String>> {
-        val createStoreRequest = CreateStore(
+        val createStoreCommand = CreateStore(
             storeId = UUID.randomUUID(),
             sellerId = sellerId,
             category = category
         )
-        return command.send(createStoreRequest)
+        return command.send(createStoreCommand)
     }
+
     @PostMapping("/seller/store/{storeId}/stock/add")
     fun addStock(
         @PathVariable
@@ -65,7 +62,7 @@ class SellerController(
         productDescription: String,
         price: Double
     ): Mono<ResponseEntity<String>> {
-        val addStockRequest = AddStock(
+        val addStockCommand = AddStock(
             storeId = storeId,
             productId = UUID.randomUUID(),
             productName = productName,
@@ -73,8 +70,9 @@ class SellerController(
             productDescription = productDescription,
             price = price
         )
-        return command.send(addStockRequest)
+        return command.send(addStockCommand)
     }
+
     @GetMapping("/seller/store/{storeId}/manage")
     fun manageStore(@PathVariable storeId: UUID): Mono<ResponseEntity<ManageStoreQueryResults>> {
         TODO()
