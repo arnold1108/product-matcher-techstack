8e86d6c700674c6e7838ca7b99a3317894b08143
opiyodhiambo <rnldlinus@gmail.com>
2023-12-21 21:39:30 +0300

    add query ingest

--- core/marketplace/src/main/kotlin/com/adventure/marketplace/ingest/QueryIngest.kt
+++ core/marketplace/src/main/kotlin/com/adventure/marketplace/ingest/QueryIngest.kt
@@ -4,5 +4,8 @@ import com.adventure.apis.marketplace.Queries.ExploreProducts
 import org.axonframework.queryhandling.QueryHandler
 
 class QueryIngest {
-
+    @QueryHandler
+    fun handle(query: ExploreProducts) {
+        TODO()
+    }
 }
