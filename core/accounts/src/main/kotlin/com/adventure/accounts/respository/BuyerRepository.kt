8b39f4f10a8978389cdff81ab247531db6b8e1db
opiyodhiambo <rnldlinus@gmail.com>
2023-12-26 13:47:42 +0300

    add seller repository and saving logic

--- core/accounts/src/main/kotlin/com/adventure/accounts/respository/BuyerRepository.kt
+++ core/accounts/src/main/kotlin/com/adventure/accounts/respository/BuyerRepository.kt
@@ -4,7 +4,6 @@ import com.adventure.accounts.entity.Buyer
 import com.adventure.apis.accounts.Commands.UserDetails
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface BuyerRepository: JpaRepository<Buyer, Long> {
