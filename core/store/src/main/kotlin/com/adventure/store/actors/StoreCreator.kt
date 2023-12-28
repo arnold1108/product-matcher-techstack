e72e7b853fb79906a09be025e6bf756d7232b53a
opiyodhiambo <rnldlinus@gmail.com>
2023-12-28 12:44:43 +0300

    define StoreCreator Actor

--- core/store/src/main/kotlin/com/adventure/store/actors/StoreCreator.kt
+++ core/store/src/main/kotlin/com/adventure/store/actors/StoreCreator.kt
@@ -1,4 +1,22 @@
 package com.adventure.store.actors
 
-class StoreCreator {
+import akka.actor.typed.Behavior
+import akka.actor.typed.javadsl.AbstractBehavior
+import akka.actor.typed.javadsl.ActorContext
+import akka.actor.typed.javadsl.Behaviors
+import akka.actor.typed.javadsl.Receive
+import com.adventure.store.model.Tasks
+
+class StoreCreator(private val context: ActorContext<Tasks>): AbstractBehavior<Tasks>(context) {
+    companion object {
+        fun create(): Behavior<Tasks> {
+            return Behaviors.setup { context ->
+                StoreCreator(context)
+            }
+        }
+    }
+
+    override fun createReceive(): Receive<Tasks> {
+        TODO("Not yet implemented")
+    }
 }
