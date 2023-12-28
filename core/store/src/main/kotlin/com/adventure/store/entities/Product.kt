25a339862c83402994c9e1af4d1d9249cfd06c76
opiyodhiambo <rnldlinus@gmail.com>
2023-12-28 11:06:45 +0300

    add products entity

---
+++ core/store/src/main/kotlin/com/adventure/store/entities/Product.kt
@@ -0,0 +1,20 @@
+package com.adventure.store.entities
+
+import org.springframework.data.cassandra.core.mapping.Column
+import org.springframework.data.cassandra.core.mapping.PrimaryKey
+import org.springframework.data.cassandra.core.mapping.Table
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Table("products")
+data class Product(
+    @PrimaryKey @Column("product_id") val productId: UUID,
+    @Column("store_id") val storeId: UUID,
+    @Column("product_name") val productName: String,
+    @Column("product_category") val productCategory: String,
+    @Column("product_description") val productDescription: String,
+    @Column("price") val price: Double,
+    @Column("quantity") val remainingQuantity: Int,
+    @Column("likes") val likes: Int,
+    @Column("time_added") val timeAdded: LocalDateTime
+)
