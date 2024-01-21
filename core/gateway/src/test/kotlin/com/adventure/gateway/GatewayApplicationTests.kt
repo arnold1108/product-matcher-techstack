69957d8a4bcf3206ddc28e077f875a0f4a41a560
opiyodhiambo <rnldlinus@gmail.com>
2024-01-21 16:39:50 +0300

    remove axonConfig

--- core/gateway/src/test/kotlin/com/adventure/gateway/GatewayApplicationTests.kt
+++ core/gateway/src/test/kotlin/com/adventure/gateway/GatewayApplicationTests.kt
@@ -4,18 +4,6 @@ import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
-//@SpringBootTest
-//class GatewayApplicationTests {
-//
-//	@Test
-//	fun contextLoads() {
-//	}
-//
-//}
-
-
-
-
 @RunWith(Cucumber::class)
 @CucumberOptions(
 	features = ["src/test/resources/features"],
