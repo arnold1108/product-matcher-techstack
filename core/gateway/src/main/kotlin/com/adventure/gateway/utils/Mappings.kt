ac730235fed72e1d5085f53254a7e518ce29125d
opiyodhiambo <rnldlinus@gmail.com>
2024-02-25 11:11:26 +0300

    update endpoints to confirm to bets practices

--- core/gateway/src/main/kotlin/com/adventure/gateway/utils/Mappings.kt
+++ core/gateway/src/main/kotlin/com/adventure/gateway/utils/Mappings.kt
@@ -3,18 +3,18 @@ package com.adventure.gateway.utils
 object Mappings {
     private const val REQUEST_MAPPING = "/soko"
 
-    const val ACCOUNT_CREATION_MAPPING = "$REQUEST_MAPPING/account/create"
+    const val ACCOUNT_CREATION_MAPPING = "$REQUEST_MAPPING/accounts/create"
 
-    const val CREATE_STORE_MAPPING = "$REQUEST_MAPPING/{seller_id}/store/create"
-    const val ADD_STOCK_MAPPING = "$REQUEST_MAPPING/{seller_id}/store/{store_id}/stock/add"
-    const val MANAGE_STORE_MAPPING = "$REQUEST_MAPPING/store/{store_id}/manage"
+    const val CREATE_STORE_MAPPING = "$REQUEST_MAPPING/{seller_id}/stores/create"
+    const val ADD_STOCK_MAPPING = "$REQUEST_MAPPING/{seller_id}/stores/{store_id}/stock/add"
+    const val MANAGE_STORE_MAPPING = "$REQUEST_MAPPING/stores/{store_id}/manage"
 
-    const val EXPLORE_PRODUCT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/explore"
-    const val LIKE_PRODUCT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/explore/{product_id}/like"
-    const val ADD_PRODUCT_TO_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/cart/add/{product_id}"
+    const val EXPLORE_PRODUCT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/products"
+    const val LIKE_PRODUCT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/products/{product_id}/like"
+    const val ADD_PRODUCT_TO_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/carts/add/{product_id}"
 
-    const val VIEW_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/cart"
-    const val REMOVE_PRODUCT_FROM_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/cart/remove/{product_id}"
-    const val CHECKOUT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/cart/checkout"
+    const val VIEW_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/carts"
+    const val REMOVE_PRODUCT_FROM_CART_MAPPING = "$REQUEST_MAPPING/{buyer_id}/carts/remove/{product_id}"
+    const val CHECKOUT_MAPPING = "$REQUEST_MAPPING/{buyer_id}/carts/checkout"
 
 }
