b5ef0c86df01a2eb96bb8cb87a559fc7b0b6ae11
opiyodhiambo <rnldlinus@gmail.com>
2024-02-25 11:21:14 +0300

    remove redundant packages

--- core/gateway/src/main/kotlin/com/adventure/gateway/controller/Account.kt
+++ core/gateway/src/main/kotlin/com/adventure/gateway/controller/Account.kt
@@ -1,16 +1,13 @@
 package com.adventure.gateway.controller
 
-import com.adventure.apis.accounts.Commands
 import com.adventure.apis.accounts.Requests.*
 import com.adventure.gateway.service.AccountService
 import com.adventure.gateway.utils.Mappings.ACCOUNT_CREATION_MAPPING
-import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
-import java.util.*
 
 @RestController
 class Account (private val account: AccountService){
